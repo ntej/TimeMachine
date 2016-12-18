ee85955e9d9a450f13ede664d281e889fba71711
ntej <ntejdev@gmail.com>
2016-12-18 17:36:43 -0600

    timeZone work

--- src/ntej/time/TimeZoneTimeGenerator.java
+++ src/ntej/time/TimeZoneTimeGenerator.java
@@ -1,8 +1,26 @@
 package ntej.time;
 
+import java.util.ArrayList;
+import java.util.TimeZone;
+
 /**
  * Created by navatejareddy on 12/18/16.
  */
 public class TimeZoneTimeGenerator {
+
+    ArrayList<String> allZonesArrayList = new ArrayList<>();
+
+
+    private ArrayList<String>  getAllZoneIds()
+    {
+        TimeZoneTimeGenerator tztg = new TimeZoneTimeGenerator();
+
+        for(String zone : TimeZone.getAvailableIDs())
+        {
+            tztg.allZonesArrayList.add(zone);
+        }
+
+        return tztg.allZonesArrayList;
+    }
 
 }
