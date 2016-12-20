7db26cd29552cdaf2778bce160857f6ed8d07e92
ntej <ntejdev@gmail.com>
2016-12-19 21:27:18 -0600

    seperated libs

--- src/ntej/time/Main.java
+++ src/ntej/time/Main.java
@@ -11,6 +11,15 @@ public class Main {
     public static void main(String[] args)
     {
 
+        String dateAndMonth;
+        String time;
+
+        UTCTimeGenerator utcTimeGenerator = new UTCTimeGenerator(System.currentTimeMillis());
+
+        dateAndMonth = utcTimeGenerator.getMonthandDate();
+        time = utcTimeGenerator.getTime();
+
+        System.out.println(dateAndMonth+" "+time);
 
     }
 }
